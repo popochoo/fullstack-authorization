53f64ebe94b40bf1000828bf2b2a675cb23eba69
popochoo <bestekst0@gmail.com>
2026-07-01 02:40:46 +0300

    update user.controller.ts

--- nestjs-server/src/user/user.controller.ts
+++ nestjs-server/src/user/user.controller.ts
@@ -1,8 +1,26 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get, HttpCode, HttpStatus, Param } from '@nestjs/common'
+import { UserRole } from 'prisma/__generated__/enums'
+
+import { Authorization } from '@/auth/decorators/auth.decorator'
+import { Authorizated } from '@/auth/decorators/authorizated.decorator'
 
 import { UserService } from './user.service'
 
-@Controller('user')
+@Controller('users')
 export class UserController {
 	constructor(private readonly userService: UserService) {}
+
+	@Authorization()
+	@HttpCode(HttpStatus.OK)
+	@Get('profile')
+	public async findProfile(@Authorizated('id') userId: string) {
+		return this.userService.findById(userId)
+	}
+
+	@Authorization(UserRole.ADMIN)
+	@HttpCode(HttpStatus.OK)
+	@Get('by-id/:id')
+	public async findById(@Param('id') id: string) {
+		return this.userService.findById(id)
+	}
 }
