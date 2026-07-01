8c8db06cf76c4a543e3a079655b5f8d4864a32cc
popochoo <bestekst0@gmail.com>
2026-07-01 08:53:48 +0300

    integrating Google reCAPTCHA v2

--- nestjs-server/src/auth/auth.controller.ts
+++ nestjs-server/src/auth/auth.controller.ts
@@ -12,17 +12,20 @@ import { Request, Response } from 'express'
 import { AuthService } from './auth.service'
 import { LoginDto } from './dto/login.dto'
 import { RegisterDto } from './dto/register.dto'
+import { Recaptcha } from '@nestlab/google-recaptcha'
 
 @Controller('auth')
 export class AuthController {
 	public constructor(private readonly authService: AuthService) {}
 
+	@Recaptcha()
 	@Post('register')
 	@HttpCode(HttpStatus.OK)
 	public async register(@Req() req: Request, @Body() dto: RegisterDto) {
 		return this.authService.register(req, dto)
 	}
 
+	@Recaptcha()
 	@Post('login')
 	@HttpCode(HttpStatus.OK)
 	public async login(@Req() req: Request, @Body() dto: LoginDto) {
